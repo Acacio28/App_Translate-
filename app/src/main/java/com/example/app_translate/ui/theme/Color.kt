a1239f7d8cd2a35903002c8a09716c2bf8777078
Akashio28 <acacioelezito933@gmail.com>
2026-04-07 22:08:10 +0900

    hadia file nebe tumpuk iha MainActivity deit no halo sai fali MVVM arkitekture KOtlin kompose

--- app/src/main/java/com/example/app_translate/ui/theme/Color.kt
+++ app/src/main/java/com/example/app_translate/ui/theme/Color.kt
@@ -2,10 +2,21 @@ package com.example.app_translate.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Warna yang Anda gunakan di TranslatorScreen
+val PurpleColor = Color(0xFF6200EE)
+val DarkPurpleColor = Color(0xFF3700B3)
+val LightPurpleColor = Color(0xFFF3E5F5)
+val WhiteColor = Color(0xFFFFFFFF)
+
+// Warna standar untuk Dark Mode
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
+// Warna standar untuk Light Mode
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val GrayColor = Color(0xFF757575)
+val LightGrayColor = Color(0xFFBDBDBD)
+val RedColor = Color(0xFFD32F2F) // Untuk menampilkan pesan error
